31e6355529da4325eef9e1066d9851ad7178f30d
Amadey <SteveDrumble1@gmail.com>
2018-04-02 13:53:51 +0300

    Authentification is renewed

--- authentification.py
+++ authentification.py
@@ -1,16 +1,37 @@
 import database as db
-def check(id: str):
-    info = db.get(id=id)
-    if info:
-        return True
-    else:
-        return False
+import user
 
 
+def check(id):
+    if id:
+        info = db.get(id=id)
+        if info:
+            return True
+        else:
+            return False
 
 
+def if_librarian(id):
+    if id:
+        librarian = db.get(id=id)
+        if isinstance(librarian, user.Librarian):
+            return True
+        else:
+            return False
+
+def check_doc(dictionary):
+    if dictionary and "type" in dictionary.keys():
+        type = dictionary["type"]
+        if type == "Book" or type == "AV" or type == "Article":
+            return dictionary["title"]
+        else:
+            return None
 
 
-
-
-
+def check_user(dictionary):
+    if dictionary and "type" in dictionary.keys():
+        type = dictionary["type"]
+        if type == "Student" or type == "VP" or type == "TA" or type == "Instructor" or type == "Professor":
+            return dictionary["id"]
+        else:
+            return None
